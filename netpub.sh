cf5959f64637286122f7600a1a59f0415b6eb3c2
Chozabu <chozabu@gmail.com>
2014-07-24 14:33:19 +0100

    update push script, page title

--- netpub.sh
+++ netpub.sh
@@ -1,2 +1,3 @@
 rsync passenger_wsgi.py alexpb@chozabu.net:kiventedserve.chozabu.net/
+rsync kee/index.html alexpb@chozabu.net:kiventedserve.chozabu.net/kee/
 ssh alexpb@chozabu.net touch kiventedserve.chozabu.net/tmp/restart.txt
